f18c9a89bc57059da3b23c8887268cde52cab15e
--global <tcbhardwaj@gmail.com>
2021-06-18 06:53:32 +0530

    Update _8_LoopDetection.java

--- src/main/java/org/practice/chapter2/linkedList/_8_LoopDetection.java
+++ src/main/java/org/practice/chapter2/linkedList/_8_LoopDetection.java
@@ -6,15 +6,15 @@ import java.util.Set;
 public class _8_LoopDetection {
     public static void main(String[] args) throws Exception {
         Node node = new Node(5);
-        Node list1 = new Node(1).append(node).append(5).append(node);
+        Node list1 = new Node(1).append(node).append(3).append(node);
 
 
         System.out.println(node);
-        Node circularNode = approach1(list1);
+        Node circularNode = solution1(list1);
         System.out.println(circularNode);
     }
 
-    public static Node approach1(Node list1) {
+    public static Node solution1(Node list1) {
         Set<Node> listSet = new HashSet<>();
         Node duplicateNode = null;
         while(list1 != null) {
@@ -26,5 +26,24 @@ public class _8_LoopDetection {
             list1 = list1.next;
         }
         return duplicateNode;
+    }
+
+    public static Node solution2(Node list) {
+        Node circularHead = null;
+        Node slow = list;
+        Node fast = list;
+
+        while(fast != null && fast.next != null){
+            if(slow == fast){
+                break;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast == null || fast.next == null){
+            return null;
+        }
+
+        return circularHead;
     }
 }
